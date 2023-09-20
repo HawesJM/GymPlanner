20e05c7d62d8585c8c934987546cbcf5e989a0a4
HawesJM <>
2023-09-20 22:01:49 +0000

    add page 2 progress functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,3 +10,14 @@ $("#nextBtn1").click(function () {
     $("#personal-info").slideUp("fast");
     $("#workoutinfo").removeClass("workout-info-next");
 });
+
+// form page 2 progress function
+
+$(".workout-type-form").click(function () {
+    $("#nextBtn2").removeClass("hiddenbutton");
+});
+
+$("#nextBtn2").click(function () {
+    $("#workoutinfo").slideUp("fast");
+    $("#membership-status").removeClass(".membership-status-hidden");
+});
